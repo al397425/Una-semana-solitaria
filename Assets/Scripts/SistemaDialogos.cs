2d895f21cc505327080cf333d000f853e37f8be0
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-10-21 23:32:52 +0200

    Introduccion al valenciano
    Ahora soporta el valenciano y ahora el fichero desde donde se lee los dialogos se puede asignar mediante el editor

--- Assets/Scripts/SistemaDialogos.cs
+++ Assets/Scripts/SistemaDialogos.cs
@@ -1,35 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using System.IO;
 using System;
 
 public class SistemaDialogos : MonoBehaviour
 {
-	public bool espaniol = false;
+	public enum Idioma {Espaniol, Ingles, Valenciano};
+	
+	//Personaje con el que se dialogara
 	public int personaje = 0;
+	//Idiomas
+	public Idioma idiomaActual = Idioma.Espaniol;
+	//CSV de los dialogos
+	public TextAsset ficheroDialogos;
+	//Texto de la ui que se modificara
+	public TextMesh textoUI;
 	
 	List<string> dialogosNPC = new List<string>();
 	List<string> dialogosPersonaje = new List<string>();
+	
+	//Obtiene la lista de dialogos usada durante la conversacion
+
     // Start is called before the first frame update
     void Start()
     {
-		using(var reader = new StreamReader(@"C:\Users\al394516\Documents\GitHub\Una-semana-solitaria\Assets\Dialogos\dialogos prueba.csv"))
-		{
-			var line = reader.ReadLine();
-			while (!reader.EndOfStream)
-			{
-				line = reader.ReadLine();
-				var values = line.Split(';');
-				if(espaniol && (values[(personaje*4)] !="" || values[(personaje*4) + 2] != "")){
-					dialogosNPC.Add(values[(personaje*4)]);
-					dialogosPersonaje.Add(values[(personaje*4) + 2]);
-				}else if(values[(personaje*4) + 1] !="" || values[(personaje*4) + 3] != ""){
-					dialogosNPC.Add(values[(personaje*4) + 1]);
-					dialogosPersonaje.Add(values[(personaje*4) + 3]);
-				}
-			}
-		}
+		//string direccion = "C:\Users\al394516\Documents\GitHub\Una-semana-solitaria\Assets\Dialogos\dialogos prueba.csv";
+
+		ObtenerListaDeDialogos();
 		
 		for(int i = 0; i< Math.Max(dialogosNPC.Count,dialogosPersonaje.Count) && (dialogosNPC[i]!="" || dialogosPersonaje[i] != ""); i++){
 			if(dialogosNPC[i]!="")
@@ -44,4 +43,38 @@ public class SistemaDialogos : MonoBehaviour
     {
         
     }
+	
+	void ObtenerListaDeDialogos(){
+		using(var reader = new StreamReader(@AssetDatabase.GetAssetPath(ficheroDialogos)))
+		{
+			var line = reader.ReadLine();
+			while (!reader.EndOfStream)
+			{
+				line = reader.ReadLine();
+				var values = line.Split(';');
+				switch(idiomaActual){
+					case Idioma.Espaniol:
+						if((values[(personaje*6)] !="" || values[(personaje*6) + 3] != "")){
+							dialogosNPC.Add(values[(personaje*6)]);
+							dialogosPersonaje.Add(values[(personaje*6) + 3]);
+						}
+					break;
+					
+					case Idioma.Ingles:
+						if(values[(personaje*6) + 1] !="" || values[(personaje*6) + 4] != ""){
+							dialogosNPC.Add(values[(personaje*6) + 1]);
+							dialogosPersonaje.Add(values[(personaje*6) + 4]);
+						}
+					break;
+					
+					case Idioma.Valenciano:
+						if(values[(personaje*6) + 2] !="" || values[(personaje*6) + 5] != ""){
+							dialogosNPC.Add(values[(personaje*6) + 2]);
+							dialogosPersonaje.Add(values[(personaje*6) + 5]);
+						}
+					break;
+				}
+			}
+		}
+	}
 }
