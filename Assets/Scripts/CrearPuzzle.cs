70e31d451e301695253b0044cb9b38ba40215d04
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-10-28 15:40:00 +0200

    Inicio

---
+++ Assets/Scripts/CrearPuzzle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrearPuzzle : MonoBehaviour
+{
+	
+	//Imagen del slot vacio donde ira la tuberia
+	public Image spriteSlotTuberia;
+	
+	Image [,]matrizSlots;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void GenerarSlots(int x, int y){
+		
+	}
+}
