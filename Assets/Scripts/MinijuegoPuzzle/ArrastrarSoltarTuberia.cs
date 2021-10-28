f658cb6a4bedf12aaaf92a7890daf6c40488de42
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-10-29 01:47:05 +0200

    Soltar objetos primera parte
    Se establece las colisiones para saber donde ha de colocarse

--- Assets/Scripts/MinijuegoPuzzle/ArrastrarSoltarTuberia.cs
+++ Assets/Scripts/MinijuegoPuzzle/ArrastrarSoltarTuberia.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ArrastrarSoltarTuberia : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+	BoxCollider2D colision;
+	Collider2D obejtoColisionado;
+	
+	
+	void Start(){
+		colision = GetComponent<BoxCollider2D>();
+	}
+
 	public void OnDrag(PointerEventData eventData){
 		transform.position = Input.mousePosition;
 		//Coge el padre y lo pone como ultima prioridad para ser renderizado el ultimo y asi la tuberia no queda detras del fondo
 		transform.parent.transform.SetAsLastSibling();
+		colision.isTrigger = true;
+		
 	}
 	
 	public void OnEndDrag(PointerEventData eventData){
-		RaycastHit hit;
-		int layerMask = 1 << 8;
-		layerMask = ~layerMask;
-		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask)){
-			 Debug.Log("Did Hit");
-		}else{
 			GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-		}
 	}
+
+
+
 }
