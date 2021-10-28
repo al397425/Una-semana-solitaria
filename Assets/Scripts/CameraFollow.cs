12b0b17ab612ff85176519b2fefad1f810cda260
Deceller <al394508@uji.es>
2021-10-28 06:55:42 +0200

    HookMovement + CameraMovement
    He realizado tanto el movimiento del gancho, como el seguimiento de la cámara a este (queda modificar para que solo lo siga en el eje Y)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   
+    public Transform target;
+    public Vector3 offset;
+
+
+    private void FixedUpdate()
+    {
+       transform.position = target.position+offset;
+    }
+
+}
