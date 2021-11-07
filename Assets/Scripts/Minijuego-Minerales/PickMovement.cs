3894fe25b62f2c6ae116d5ecfb5bb91611e61440
lafloba <90605843+lafloba@users.noreply.github.com>
2021-11-07 20:48:59 +0100

    Primer Intento Movimiento Pico

--- Assets/Scripts/Minijuego-Minerales/PickMovement.cs
+++ Assets/Scripts/Minijuego-Minerales/PickMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PickMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10f;
+    Vector2 lastClickedPos;
+    bool moving;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+
+        if(moving && (Vector2)transform.position != lastClickedPos)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+        } else
+        {
+            moving = false;
+        }
     }
 }
