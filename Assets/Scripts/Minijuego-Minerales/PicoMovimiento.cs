fdd51e1a2c453a5be2059a6e3469def07950b6d4
lafloba <90605843+lafloba@users.noreply.github.com>
2021-11-08 10:47:50 +0100

    Movimiento Pico

--- Assets/Scripts/Minijuego-Minerales/PicoMovimiento.cs
+++ Assets/Scripts/Minijuego-Minerales/PicoMovimiento.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickMovement : MonoBehaviour
-{
+public class PicoMovimiento : MonoBehaviour{
     public float speed = 10f;
     Vector2 lastClickedPos;
     bool moving;
 
-    private void Update()
-    {
+    private void Update(){
         if (Input.GetMouseButtonDown(0))
         {
             lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             moving = true;
         }
 
-        if(moving && (Vector2)transform.position != lastClickedPos)
+        if (moving && (Vector2)transform.position != lastClickedPos)
         {
             float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
-        } else
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos,
+                step);
+        }
+        else
         {
             moving = false;
         }
