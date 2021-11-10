24d7b89786604143f9b081723f03d92899a71366
lafloba <90605843+lafloba@users.noreply.github.com>
2021-11-10 11:06:32 +0100

    Texto Puntos

--- Assets/Scripts/Minijuego-Minerales/Puntos.cs
+++ Assets/Scripts/Minijuego-Minerales/Puntos.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Puntos : MonoBehaviour
 {
     public int puntos;
+    public Text TXTpuntos;
+
+    private void Update()
+    {
+        TXTpuntos.text = "Puntos: " + puntos.ToString();
+    }
 }
