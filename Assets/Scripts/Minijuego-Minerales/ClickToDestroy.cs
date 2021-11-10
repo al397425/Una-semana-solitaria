9bf9798c58e141b333b62e421bd97b69e052aa19
lafloba <90605843+lafloba@users.noreply.github.com>
2021-11-10 10:52:35 +0100

    Script Puntos

--- Assets/Scripts/Minijuego-Minerales/ClickToDestroy.cs
+++ Assets/Scripts/Minijuego-Minerales/ClickToDestroy.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickToDestroy : MonoBehaviour
 {
+    public GameObject ObjPuntos;
+
+
     void OnMouseDown()
     {
+        ObjPuntos.GetComponent<Puntos>().puntos += 10;
         Destroy(gameObject);
     }
+
 }
