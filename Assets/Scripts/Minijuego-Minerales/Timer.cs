f60fe6b505457d5ccb02d1b1f2f5de84b6808e61
lafloba <90605843+lafloba@users.noreply.github.com>
2021-11-16 19:55:44 +0100

    Boton Reinicio

--- Assets/Scripts/Minijuego-Minerales/Timer.cs
+++ Assets/Scripts/Minijuego-Minerales/Timer.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Timer : MonoBehaviour
 {
     public Text tiempoText;
     public float tiempo = 0.0f;
+    
 
     public void Update(){
         tiempo -= Time.deltaTime;
         tiempoText.text = "" + tiempo.ToString("f0");
+
+        if (tiempo <= 0){
+            Destroy(gameObject);
+        }
+
     }
 }
