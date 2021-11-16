3485409de296afab9516a27a3421f385f158fc88
lafloba <90605843+lafloba@users.noreply.github.com>
2021-11-16 21:51:46 +0100

    reset

---
+++ Assets/Scripts/Minijuego-Minerales/controlNivel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlNivel : MonoBehaviour
+{
+    public void Minerales(){
+        SceneManager.LoadScene("Minijuego-Minerales");
+    }
+
+}
