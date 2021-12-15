ff37afbe3d9bca07f497992753de239787ec6438
Carlos Gómez Martínez <al394508@uji.es>
2021-12-15 10:29:11 +0100

    Lista de cosas a pulir casi completada

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
+    public AudioSource audioDataCoin;
     private void OnTriggerEnter2D(Collider2D col)
     {
         switch (col.tag)
         {
             case "obstaculo-pescar":
+                if (!audioDataCoin.isPlaying)
+                    audioDataCoin.Play(0);
                 this.gameObject.SetActive(false);
                 break;
         }
