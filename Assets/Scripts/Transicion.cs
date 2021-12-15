014dd937c06a8ddc623147651135ac6468f3c604
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-12-15 03:25:03 +0100

    Movimiento camara

--- Assets/Scripts/Transicion.cs
+++ Assets/Scripts/Transicion.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ using Cinemachine;
+ 
 public class Transicion : MonoBehaviour
 {
     GameObject jugador;
@@ -9,6 +10,8 @@ public class Transicion : MonoBehaviour
     Animator anim;
     public GameObject nuevaPosicionJugador;
     public GameObject objetoTransicion;
+    public PolygonCollider2D confinadorCamara;
+    public CinemachineConfiner  camaraCineMachine;
     public float delay = 0.5f;
 
    void OnTriggerEnter2D(Collider2D other){
@@ -22,6 +25,7 @@ public class Transicion : MonoBehaviour
 
    IEnumerator establecerPosicionJugador(){
        yield return new WaitForSeconds(delay);
+       camaraCineMachine.m_BoundingShape2D = confinadorCamara;
        jugador.transform.position = nuevaPosicionJugador.transform.position;
        objetoTransicion.GetComponent<EjecutarFundido>().EjecutarFadeOut();
         jugador.GetComponent<MovementCharacter>().enabled = true;
